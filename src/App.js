00eec3005b7c26802e5944e6d1b9c657f1afd845
Lucas Tembras <51347268+lucastemb@users.noreply.github.com>
2024-02-04 23:35:45 -0500

    Added background and made subtle improvements to the design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,23 @@
 import './App.css';
 import Countdown from 'react-countdown';
+import cfc from './Code_for_Change_Logo.png';
 
 function App() {
   return (
     <div className="App">
-      <p className="text"> Time Until Code for Change </p>
+      <night-sky
+        id="nightSky"
+        layers="4"
+        density="30"
+        velocity="60"
+        starcolor="#FFF">
+      
+      </night-sky> 
+      <div className="timer-div"> 
+      <img className="logo" src = {cfc}/>
+      <p className="text"> Time Until Code for Change: </p>
       <Countdown className="timer" date={'2024-02-10T09:00:00'}/>
+      </div>
     </div>
   );
 }
